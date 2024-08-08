fddbf4fb54f95cb74b17cea4569a4d8fb8bcaa50
Mycroft180202 <111584594+Mycroft180202@users.noreply.github.com>
2024-08-08 14:33:57 +0700

    Create default folder

---
+++ QuanLiQuanCaphe/Models/Report.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace QuanLiQuanCaphe.Models;
+
+public partial class Report
+{
+    public int ReportId { get; set; }
+
+    public string ReportName { get; set; } = null!;
+
+    public DateTime ReportDate { get; set; }
+
+    public string FilePath { get; set; } = null!;
+}
---
+++ QuanLiQuanCaphe/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace QuanLiQuanCaphe.Models;
+
+public partial class User
+{
+    public int UserId { get; set; }
+
+    public string Username { get; set; } = null!;
+
+    public string PasswordHash { get; set; } = null!;
+
+    public string Role { get; set; } = null!;
+
+    public virtual ICollection<Cart> Carts { get; set; } = new List<Cart>();
+
+    public virtual ICollection<Order> Orders { get; set; } = new List<Order>();
+}
