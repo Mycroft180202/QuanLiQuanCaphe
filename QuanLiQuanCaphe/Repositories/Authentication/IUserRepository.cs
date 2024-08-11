6838c2b1aa58dff15de599e544fae88d8ba311dd
Mycroft180202 <111584594+Mycroft180202@users.noreply.github.com>
2024-08-11 21:15:04 +0700

    add repo

--- QuanLiQuanCaphe/Repositories/Authentication/IUserRepository.cs
+++ QuanLiQuanCaphe/Repositories/Authentication/IUserRepository.cs
@@ -12,7 +12,9 @@ namespace QuanLiQuanCaphe.Repositories.Authentication
         List<User> GetAllUsers();
         void AddUser(User user);
         void UpdateUser(User user);
-        void DeleteUser(User user);
-        void GetUserByUsernameAndPassword(string username, string password);
+        void DeleteUser(int userId);
+        User GetUserByUsername(string username);
+        void AddUser(User newUser);
+        User GetUserByUsernameAndPassword(string username, string password);
     }
 }
